cf0265cfeb1d106ad3c1eca6422e64af6a97e259
kyeongh0324 <wkftodrlsrudghksdl@gmail.com>
2025-08-24 10:52:28 +0900

    백엔드 서버 주소 최종 연결

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function sendMessage() {
 
     try {
         // ⬇️ 백엔드 서버의 전체 주소를 정확하게 입력합니다 ⬇️
-        const response = await fetch('http://127.0.0.1:5000/chat', {
+        const response = await fetch('http://kyeongh0324.pythonanywhere.com/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
